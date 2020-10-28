35c7d126228aa685c71c88aa75ef51dabe9a54c6
bir-b <vcbenn@gmail.com>
2020-10-28 23:26:59 +1100

    added basic styling to background of page

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function addTask() {
-    let val = $("#taskInput").val();
+    let val = $("#task-input").val();
     if(!val) {
         console.log("Input empty!");
     } else {
         //function to create task...
         val.trim();
-        $("#currentTasks").append("<p>" + val + "</p>");
+        $("#current-tasks").append("<p>" + val + "</p>");
     }
 }
